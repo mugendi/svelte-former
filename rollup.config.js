46ca555ede065c206f68278b70ea2cbe67546587
Anthony Mugendi <ngurumugz@gmail.com>
2024-05-03 15:22:42 +0300

    added minification for production

--- rollup.config.js
+++ rollup.config.js
@@ -12,52 +12,55 @@ import hmr from 'rollup-plugin-reloadsite';
 import commonjs from '@rollup/plugin-commonjs';
 import styles from 'rollup-plugin-styles';
 import terser from '@rollup/plugin-terser';
+import del from 'rollup-plugin-delete';
 
 const production = !process.env.ROLLUP_WATCH;
 
 export default {
-    // This `main.js` file we wrote
-    input: 'src/index.js',
-    output: {
-        // The destination for our bundled JavaScript
-        file: 'build/svelte-former.js',
-        // Our bundle will be an Immediately-Invoked Function Expression
-        format: 'iife',
-        // The IIFE return value will be assigned into a variable called `app`
-        name: 'SvelteFormer',
-        sourcemap: !production ? 'inline' : false,
-    },
-    plugins: [
-        styles(),
+  // This `main.js` file we wrote
+  input: 'src/index.js',
+  output: {
+    // The destination for our bundled JavaScript
+    file: production ? 'build/svelte-former.min.js' : 'build/svelte-former.js',
+    // Our bundle will be an Immediately-Invoked Function Expression
+    format: 'iife',
+    // The IIFE return value will be assigned into a variable called `app`
+    name: 'SvelteFormer',
+    sourcemap: !production ? 'inline' : false,
+  },
+  plugins: [
+    production && del({ targets: 'build/*' }),
 
-        // Tell any third-party plugins that we're building for the browser
-        resolve({
-            browser: true,
-            dedupe: ['svelte'],
-            exportConditions: ['svelte'],
-        }),
+    styles(),
 
-        commonjs(),
+    // Tell any third-party plugins that we're building for the browser
+    resolve({
+      browser: true,
+      dedupe: ['svelte'],
+      exportConditions: ['svelte'],
+    }),
 
-        svelte({
-            // Tell the svelte plugin where our svelte files are located
-            include: 'src/**/*.svelte',
-        }),
+    commonjs(),
 
-        production && terser({ output: { comments: false } }),
+    svelte({
+      // Tell the svelte plugin where our svelte files are located
+      include: 'src/**/*.svelte',
+    }),
 
-        !production &&
-            serve({
-                open: true,
-                host: 'localhost',
-                port: 10001,
-                openPage: '/examples/index.html',
-            }),
+    production && terser({ output: { comments: false } }),
 
-        !production &&
-            hmr({
-                dirs: ['./build', './examples'],
-                filter: '**/svelte-former.js',
-            }),
-    ],
+    !production &&
+      serve({
+        open: true,
+        host: 'localhost',
+        port: 10001,
+        openPage: '/examples/index.html',
+      }),
+
+    !production &&
+      hmr({
+        dirs: ['./build', './examples'],
+        filter: '**/svelte-former.js',
+      }),
+  ],
 };
